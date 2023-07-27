6211b708b25ece4c4b7c6c25634a80299edbe41c
unknown <merryskachr@gmail.com>
2023-07-27 15:09:22 +0800

    fixed auto add product data

--- models/product.model.js
+++ models/product.model.js
@@ -23,12 +23,14 @@ const Product = mongoose.model('Product', productSchema)
 
 const countData = getAllProducts().then(data => {
   if(data.length<1){
-    const addData = new Product({
+    const addData = new Product(
+      {
       link_product: "https://nodejs.org/en",
       title:"NodeJS installer",
       price:50000,
       videoID: '64c21000258b2c61c16a576e'
-    }).save().then(data => console.log("data product inserted"))
+    }
+    ).save().then(data => console.log("data product inserted"))
   }
 })
 
